90959696c0f0736b0d1a91c04ffac297e0398a52
andermatt <matt10.anderson@gmail.com>
2018-10-10 17:12:46 -0400

    Refactored Adventurer card.

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -655,9 +655,6 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
 
   int tributeRevealedCards[2] = {-1, -1};
   int temphand[MAX_HAND];// moved above the if statement
-  int drawntreasure=0;
-  int cardDrawn;
-  int z = 0;// this is the counter for the temp hand
   if (nextPlayer > (state->numPlayers - 1)){
     nextPlayer = 0;
   }
@@ -667,25 +664,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   switch( card ) 
     {
     case adventurer:
-      while(drawntreasure<2){
-	if (state->deckCount[currentPlayer] <1){//if the deck is empty we need to shuffle discard and add to deck
-	  shuffle(currentPlayer, state);
-	}
-	drawCard(currentPlayer, state);
-	cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];//top card of hand is most recently drawn card.
-	if (cardDrawn == copper || cardDrawn == silver || cardDrawn == gold)
-	  drawntreasure++;
-	else{
-	  temphand[z]=cardDrawn;
-	  state->handCount[currentPlayer]--; //this should just remove the top card (the most recently drawn one).
-	  z++;
-	}
-      }
-      while(z-1>=0){
-	state->discard[currentPlayer][state->discardCount[currentPlayer]++]=temphand[z-1]; // discard all cards in play that have been drawn
-	z=z-1;
-      }
-      return 0;
+        return playAdventurer(state);
 			
     case council_room:
       //+4 Cards
@@ -1326,6 +1305,46 @@ int updateCoins(int player, struct gameState *state, int bonus)
   state->coins += bonus;
 
   return 0;
+}
+
+
+/*
+ * Refactored card effects.
+ */
+
+int playAdventurer(struct gameState *state) {
+    int drawnTreasure = 0;
+    int tempHandIndex = 0;
+    int currentPlayer = whoseTurn(state);
+
+    int cardDrawn;
+    int tempHand[MAX_HAND];
+
+
+    while (drawnTreasure < 2) {
+        if (state->deckCount[currentPlayer] < 1) {
+            shuffle(currentPlayer, state);
+        }
+
+        drawCard(currentPlayer, state);
+        cardDrawn = state->hand[currentPlayer][state->handCount[currentPlayer]-1];
+        if (cardDrawn == copper ||
+            cardDrawn == silver ||
+            cardDrawn == gold) {
+            drawnTreasure++;
+        } else {
+            tempHand[tempHandIndex] = cardDrawn;
+            state->handCount[currentPlayer]--;
+            tempHandIndex++;
+        }
+    }
+
+    while (tempHandIndex-1 > 0) {
+        state->discard[currentPlayer][state->discardCount[currentPlayer]++]=tempHand[tempHandIndex-1];
+        tempHandIndex--;
+    }
+
+    return 0;
 }
 
 

99fd9abd9184d523a4710550e82c5e5fc622164d
andermatt <matt10.anderson@gmail.com>
2018-10-10 17:39:10 -0400

    Refactored Treasure Map card.

--- dominion/dominion.c
+++ dominion/dominion.c
@@ -649,7 +649,6 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
   int j;
   int k;
   int x;
-  int index;
   int currentPlayer = whoseTurn(state);
   int nextPlayer = currentPlayer + 1;
 
@@ -1133,34 +1132,7 @@ int cardEffect(int card, int choice1, int choice2, int choice3, struct gameState
       return 0;
 		
     case treasure_map:
-      //search hand for another treasure_map
-      index = -1;
-      for (i = 0; i < state->handCount[currentPlayer]; i++)
-	{
-	  if (state->hand[currentPlayer][i] == treasure_map && i != handPos)
-	    {
-	      index = i;
-	      break;
-	    }
-	}
-      if (index > -1)
-	{
-	  //trash both treasure cards
-	  discardCard(handPos, currentPlayer, state, 1);
-	  discardCard(index, currentPlayer, state, 1);
-
-	  //gain 4 Gold cards
-	  for (i = 0; i < 4; i++)
-	    {
-	      gainCard(gold, state, 1, currentPlayer);
-	    }
-				
-	  //return success
-	  return 1;
-	}
-			
-      //no second treasure_map found in hand
-      return -1;
+      return playTreasureMap(state, handPos);
     }
 	
   return -1;
@@ -1352,6 +1324,31 @@ int playVillage(struct gameState *state, int handPos) {
   discardCard(handPos, currentPlayer, state, 0);
 
   return 0;
+}
+
+int playTreasureMap(struct gameState *state, int handPos) {
+  int currentPlayer = whoseTurn(state);
+  int treasureMapHandIndex = -1;
+  int retVal = -1;
+
+  for (int i = 0; i < state->handCount[currentPlayer]; i++) {
+    if (state->hand[currentPlayer][i] == treasure_map && treasureMapHandIndex != handPos) {
+      treasureMapHandIndex = i;
+      break;
+    }
+  }
+
+  if (treasureMapHandIndex > -1) {
+    discardCard(handPos, currentPlayer, state, 1);
+    discardCard(treasureMapHandIndex, currentPlayer, state, 1);
+
+    for (int i = 0; i < 4; i++) {
+      gainCard(gold, state, 1, currentPlayer);
+      retVal = 1;
+    }
+  }
+
+  return retVal;
 }
 
 //end of dominion.c

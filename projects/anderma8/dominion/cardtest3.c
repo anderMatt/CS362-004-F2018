0738e081f7cac98fa82598d3077680f5c34108b5
andermatt <matt10.anderson@gmail.com>
2018-10-22 15:50:08 -0400

    Expanded Great Hall unit tests.

--- projects/anderma8/dominion/cardtest3.c
+++ projects/anderma8/dominion/cardtest3.c
@@ -7,6 +7,7 @@ CS 362 - Assignment 3
 #include <stdio.h>
 #include <string.h>
 #include "dominion.h"
+#include "testhelpers.h"
 
 /*
  * Unit tests for Great Hall
@@ -37,23 +38,6 @@ void init_state(struct gameState *state) {
     memcpy(state->hand, hand, sizeof(hand));
 }
 
-int hand_contains(struct gameState *state, int player, int card) {
-    for(int i = 0; i < state->handCount[player]; i++) {
-        if (state->hand[player][i] == card) {
-            return 1;  // Contains card.
-        }
-    }
-
-    return 0;  // Does not contain card.
-}
-
-void report_result(int expected, int actual, char *errMessage) {
-    if (expected == actual) {
-        puts("\tPassed.");
-    } else {
-        printf("\tFailed. %s\n", errMessage);
-    }
-}
 
 int main(int argn, char **argv) {
     struct gameState state;
@@ -73,16 +57,23 @@ int main(int argn, char **argv) {
     expected = 1;
     actual = (state.numActions == 1);
 
-    puts("One action gained.");
+    puts("Player gains one action gained.");
     char errMsg[100];
     snprintf(errMsg, 100, "Expected to gain one action. Actually gained: %i actions", (state.numActions));  // Started with 0 actions.
     report_result(expected, actual, errMsg);
 
-    puts("One card drawn.");
+
+    puts("Player draws one card.");
     memset(errMsg, '\0', 100);
     int handSize = sizeof(state.handCount[player]) / sizeof(int);
     snprintf(errMsg, 100, "Expected hand count: %i, Actual hand count: %i", 1, state.handCount[player]);
     actual = (handSize == 1);
+    report_result(expected, actual, errMsg);
+
+    puts("\t Card was drawn from active player's deck.");
+    expected = 5;
+    actual = (state.deckCount[player]);
+    snprintf(errMsg, 100, "\tExpected deck count: %i, Actual deck count: %i", expected, actual);
     report_result(expected, actual, errMsg);
 
     return 0;

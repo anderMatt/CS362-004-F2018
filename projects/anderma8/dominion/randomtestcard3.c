2a40ba3ac3ab0f94fdb0b709a9be8f915a78eafe
andermatt <matt10.anderson@gmail.com>
2018-11-12 10:31:41 -0500

    Added non-turn player tests.

--- projects/anderma8/dominion/randomtestcard3.c
+++ projects/anderma8/dominion/randomtestcard3.c
@@ -34,16 +34,43 @@ int main(int argn, char **argv) {
 
     int startingHandSize = state.handCount[player];
     int startingDeckSize = state.deckCount[player];
+    int startingDiscardSize = state.discardCount[player];
+    int startingHandSize2 = state.handCount[player+1];
+    int startingDeckSize2 = state.deckCount[player+1];
+    int startingDiscardSize2 = state.discardCount[player+1];
 
     cardEffect(adventurer, 0, 0, 0, &state, 0, NULL);
     int afterHandSize = state.handCount[player];
     int expectedHandSize = startingHandSize + 2;  // Draw two net cards.
     report_result(expectedHandSize, afterHandSize, "\t\tExpected hand to contain %i cards. Actually contains %i cards.\n");
 
+    puts("\t Cards drawn from turn player's deck.");
     // Make sure cards were drawn from the turn player's deck.
     int afterDeckSize = state.deckCount[player];
-    int expectedDeckSize = startingDeckSize - 10;  // Based on indices we added treasures to the deck.
+    int expectedDeckSize = startingDeckSize - 7;  // Based on indices we added treasures to the deck.
     report_result(expectedDeckSize, afterDeckSize, "\t\tExpected deck to contain %i cards. Actually contains %i cards.\n");
+
+    puts("\t Non-treasures added to turn player's discard pile.");
+    // Make sure non-treasure cards were properly discarded.
+    int afterDiscardSize = state.discardCount[player];
+    int expectedDiscardSize = startingDiscardSize + 7;
+    report_result(expectedDiscardSize, afterDiscardSize, "\t\tExpected discard pile to contain %i cards. Actually contains %i cards.\n");
+
+
+    // Make sure there weren't any unintended side effects for the non-turn player.
+    puts("\t No unintended side effects for non-turn player state.");
+    // Make sure non turn-player's hand, deck, and discard piles remain unchanged.
+    puts("\t Non turn-player's deck remains unchanged.");
+    int afterDeckSize2 = state.deckCount[player+1];
+    report_result(startingDeckSize2, afterDeckSize2, "\t\tExpected non-turn player's deck to contain %i cards. Actually contains %i cards.\n");
+
+    puts("\t Non turn-player's hand remains unchanged.");
+    int afterHandSize2 = state.handCount[player+1];
+    report_result(startingHandSize2, afterHandSize2, "\t\tExpected non-turn player's hand to contain %i cards. Actually contains %i cards.\n");
+
+    puts("\t Non turn-player's discard pile unchanged.");
+    int afterDiscardSize2 = state.discardCount[player+1];
+    report_result(startingDiscardSize2, afterDiscardSize2, "\t\tExpected non-turn player's discard pile to contain %i cards. Actually contains %i cards.\n");
 
 
     return 0;

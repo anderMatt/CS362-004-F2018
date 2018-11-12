46a192d4e93b76d986ea90056f8c3e8c16addfd3
andermatt <matt10.anderson@gmail.com>
2018-11-12 09:28:22 -0500

    Begin writing Adventurer random tests.

--- projects/anderma8/dominion/testhelpers.c
+++ projects/anderma8/dominion/testhelpers.c
@@ -1,6 +1,6 @@
 /**************************************************
  * Matthew Anderson
- * 10/31/18
+ * 11/12/18
  * CS 362 Assignment 4
  * 
  * Helper functions for assignment 4.
@@ -55,6 +55,51 @@ void game_state_initialize_random(struct gameState *state) {
     int deckSize = sizeof(state->deckCount[0]) / sizeof(int);
     state->deckCount[0] = deckSize;
 
+}
+
+/*
+Inititalizes game state with pre-determined state variables. Initializes
+more state variables than game_state_initialize_random.
+
+Code adapted from class lectures.
+*/
+void game_state_inititalize_random_full(struct gameState *state) {
+    // Clear existing state.
+    memset(state, 0, sizeof(struct gameState));
+    int kingdomCards[10];
+    for(int i = 0; i < 10; i++) {
+        kingdomCards[i] = get_random_dominion_card();  // No teasures or victory cards - we'll add these on test-by-test basis.
+    }
+    initializeGame(2, kingdomCards, 100, state);
+
+    // Player deck
+    int playerDeck[10];
+    for (int j = 0; j < 10; j++) {
+        playerDeck[j] = council_room + j;
+    }
+
+    // Player discard pile
+    int playerDiscard[5];
+    for (int k = 0; k < 5; k++) {
+        playerDiscard[k] = great_hall + k;
+    }
+
+
+    // Now, set the game state variables.
+    state->handCount[0] = 10;
+    state->deckCount[0] = 10;
+    state->discardCount[0] = 5;
+    memcpy(state->hand[0], kingdomCards, sizeof(int) * state->handCount[0]);
+    memcpy(state->deck[0], playerDeck, sizeof(int) * state->deckCount[0]);
+    memcpy(state->discard[0], playerDiscard, sizeof(int) * 5);
+
+    // Now, set the second player's state variables
+    state->handCount[1] = 10;
+    state->deckCount[1] = 10;
+    state->discardCount[1] = 5;
+    memcpy(state->hand[1], kingdomCards, sizeof(int) * state->handCount[1]);
+    memcpy(state->deck[1], playerDeck, sizeof(int) * state->deckCount[1]);
+    memcpy(state->discard[1], playerDiscard, sizeof(int) * 5);
 }
 
 void report_result(int expected, int actual, char *errMsg) {
